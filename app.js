b2805c7eb84bb7a7e28eee866476d22a28604827
Ivy Swan <ivyswan@Ivys-MacBook-Pro.local>
2019-11-05 12:07:03 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req,res){
+    res.render("index");
+});
+app.get("/startcoding", function(req,res){
+    res.render("startcoding");
+});
+app.get("/webcourses", function(req,res){
+    res.render("webcourses");
+});
+app.get("/academyclass", function(req,res){
+    res.render("academyclass");
+});
+app.get("/webdevelopment", function(req,res){
+    res.render("webdevelopment");
+});
+app.get("/portfolio", function(req,res){
+    res.render("portfolio");
+});
+
+app.listen(3000, function(){
+    console.log("Server is listening!!!"); 
+});
---
+++ public/scripts/script.js
@@ -0,0 +1,16 @@
+// Navbar scroll colour change
+$(window).scroll(function(){
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+});
+
+// Makes the menu background-color darker when menu icon is clicked
+var menuIcon = document.querySelector(".navbar-toggler");
+var transparentMenu = document.querySelector("#navbar");
+menuIcon.addEventListener("click",function(){
+	transparentMenu.classList.toggle("darkMenu");
+});
+
+// Transforms menu icon to X
+function menuFunction(x) {
+	x.classList.toggle("menuChange");
+  }
